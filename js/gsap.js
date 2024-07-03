47e477cbe6b97280e8f8dd6759928ed725d6c1b6
ssupix <151896598+ssupix@users.noreply.github.com>
2024-07-02 18:38:34 -0700

    nav-bar desktop

---
+++ js/gsap.js
@@ -0,0 +1,12 @@
+// RUNNING TEXT LINE
+// animates elements with the class .running ul li
+gsap.to(".running-line ul li", {
+    // set the animation duration to 6 seconds
+    duration: 4,
+    // moves the elements horizontally by -102%
+    x: "-105%",
+    // linear easing
+    ease: "linear",
+    // makes the animation repeat infinitely
+    repeat: -1
+});
