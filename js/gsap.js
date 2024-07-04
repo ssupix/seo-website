23c0ce659c70c1da58bcc25d45b50bf4d3873c14
ssupix <151896598+ssupix@users.noreply.github.com>
2024-07-03 20:40:14 -0700

    running line and hero

--- js/gsap.js
+++ js/gsap.js
@@ -11,6 +11,19 @@ gsap.to(".running-line ul li", {
     repeat: -1
 });
 
+// RUNNING TEXT LINE
+// animates elements with the class .running ul li
+gsap.to(".issue-line ul li", {
+    // set the animation duration to 6 seconds
+    duration: 4,
+    // moves the elements horizontally by -102%
+    x: "-105%",
+    // linear easing
+    ease: "linear",
+    // makes the animation repeat infinitely
+    repeat: -1
+});
+
 gsap.registerPlugin(ScrollTrigger)
 
 ScrollTrigger.create({
